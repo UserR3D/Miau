7872aa532de94681feacbc9ef4897d999838c73c
Kauã <kauawar04@gmail.com>
2023-03-30 17:00:27 -0300

    responsivo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import ScrollAnima from "./modules/scroll.js";
+import SlideAnima from "./modules/slide.js";
 
 const scroll = new ScrollAnima('[data-anime="scroll"]');
 scroll.init();
+
+const slide = new SlideAnima("[data-slide='slide']", '[data-anima="slide"]');
+slide.init();
